9621c6546829fe0236f225ba716c52baabb2fb21
Jessica Luhahe <jessica.luhahe@hotmail.fr>
2021-12-14 17:36:40 +0100

    commit locations  avant merge

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,12 @@ class LocationsController < ApplicationController
   def show
   end
 
+  def new
+    @planet = Planet.find(params[planet_id])
+    @user = User.find(params[user_id])
+    @location = Location.new
+  end
+
   def create
   end
 end
