0af24bd0e0aa2ba49e6e3a713a89bbf8e5bd2d70
aurelie taschon <aurelie.taschon@gmail.com>
2021-12-14 16:10:47 +0100

    end seeds - modif modele

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,13 +1,13 @@
 class Planet < ApplicationRecord
-  LOCALISATIONPLANET = ['Centre', 'Bordure-Intérieure', 'Bordure-Extérieure', 'Régions-Inconnues']
-  TYPEPLANET = ['Tellurique', 'Gazeuse', 'Cité', 'Désert', 'Forêt', 'Aquatique', 'Sauvage', 'Glacée', 'Mixte']
+  LOCALISATIONPLANET = ['Centre', 'Bordure Intérieure', 'Bordure Extérieure', 'Régions Inconnues']
+  TYPEPLANET = ['Gazeuse', 'Cité', 'Désert', 'Forêt', 'Aquatique', 'Sauvage', 'Glacée', 'Mixte']
   SAFETYPLANET = [1, 2, 3]
   belongs_to :user
   has_many :locations, dependent: :destroy
   validates :name, presence: true
-  validates :type, presence: true, collection: Planet::TYPEPLANET
-  validates :safety, presence: true, collection: Planet::SAFETYPLANET
+  validates :type, presence: true, inclusion: { in: TYPEPLANET }
+  validates :safety, presence: true, inclusion: { in: SAFETYPLANET }
   validates :prix, presence: true
   validates :description, presence: true
-  validates :localisation, presence: true, collection: Planet::LOCALISATIONPLANET
+  validates :localisation, presence: true, inclusion: { in: LOCALISATIONPLANET }
 end
