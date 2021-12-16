ac448149c6fa6cf8e6dbe50e610e5966cc4bccdb
GUOLE JING <guole.jing@gmail.com>
2021-12-16 17:47:52 +0100

    avancement sur map, fini la journee

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,27 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat([32.99038215364821, -106.97543468269595])
+      .addTo(map);
+
+    fitMapToMarkers(map, markers);
+  }
+    const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
+};
+
+export { initMapbox };
