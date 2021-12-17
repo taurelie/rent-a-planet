89dc33c9d2a155a0dddfd75ff8d9e9314aefcbc5
GUOLE JING <guole.jing@gmail.com>
2021-12-17 15:38:50 +0100

    global map + marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,7 +8,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/lola521/ckxaac7klbqxl14o4fjjvcn86'
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -17,13 +17,6 @@ const initMapbox = () => {
         .setLngLat([marker.lng, marker.lat])
         .addTo(map);
     });
-
-    fitMapToMarkers(map, markers);
-  }
-    const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 };
 
